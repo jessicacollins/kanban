1df3cdfb5e478b71dd3757841623ebad4855bc0c
Jessica Collins <jessica.collins@recruiting.com>
2015-12-21 14:19:51 -0700

    Added flux and persist data to local storage

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,27 +1,24 @@
 import uuid from 'node-uuid';
 import React from 'react';
 import Notes from './Notes.jsx';
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      notes: [
-        {
-          id: uuid.v4(),
-          task: 'Learn Webpack'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Learn React'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Do laundry'
-        }
-      ]
-    };
+    this.state = NoteStore.getState();
+  }
+
+  componentDidMount() {
+    NoteStore.listen(this.storeChanged);
+  }
+  componentWillUnmount() {
+    NoteStore.unlisten(this.storeChanged);
+  }
+  storeChanged = (state) => {
+    this.setState(state);
   }
 
   render() {
@@ -37,29 +34,13 @@ export default class App extends React.Component {
     );
   }
 
-  addNote = () => {
-  	this.setState({
-  		notes: this.state.notes.concat([{
-  			id: uuid.v4(),
-  			task: 'New task'
-  		}])
-  	});
+  addNote() {
+    NoteActions.create({task: 'New task'});
   }
-
-  editNote = (id, task) => {
-    const notes = this.state.notes.map((note) => {
-      if(note.id === id) {
-        note.task = task;
-      }
-      return note;
-    });
-
-    this.setState({notes});
+  editNote(id, task) {
+    NoteActions.update({id, task});
   }
-
-  deleteNote = (id) => {
-    this.setState({
-      notes: this.state.notes.filter((note) => note.id !== id)
-    });
+  deleteNote(id) {
+    NoteActions.delete(id);
   }
 }
