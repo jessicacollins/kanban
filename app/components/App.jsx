ba74190eb10e2415452371e58d073cd7ff753ea6
Jessica Collins <jessica.collins@recruiting.com>
2016-01-14 15:13:05 -0700

    Implemented Alt Container

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,35 +1,26 @@
-import uuid from 'node-uuid';
+// import uuid from 'node-uuid';
+import AltContainer from 'alt-container';
 import React from 'react';
 import Notes from './Notes.jsx';
 import NoteActions from '../actions/NoteActions';
 import NoteStore from '../stores/NoteStore';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = NoteStore.getState();
-  }
-
-  componentDidMount() {
-    NoteStore.listen(this.storeChanged);
-  }
-  componentWillUnmount() {
-    NoteStore.unlisten(this.storeChanged);
-  }
-  storeChanged = (state) => {
-    this.setState(state);
-  }
 
   render() {
-    const notes = this.state.notes;
 
     return (
       <div>
       	<button className="add-note" onClick={this.addNote}>+</button>
-        <Notes items={notes} 
-        onEdit={this.editNote} 
-        onDelete={this.deleteNote} />
+
+        <AltContainer
+          store={[NoteStore]}
+          inject={[
+            notes: () => NoteStore..getState().notes
+          ]}
+        >
+          <Notes onEdit={this.editeNote} onDelete={this.deleteNote}/>
+        </AltContainer>
       </div>
     );
   }
